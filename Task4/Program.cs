15213edb091536001ff1fdb61065861ee19c97ab
sulagaev_dmitry <sulagaevdim@mail.ru>
2024-02-06 17:23:02 +0400

    add Lesson1/Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,23 @@
+﻿int a = 5;
+int b = 7;
+int c = 4;
+int d = 9;
+int e = 2;
+int max = a;
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+Console.WriteLine(max);
